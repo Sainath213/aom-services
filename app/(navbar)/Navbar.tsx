daf682e6d191572c39d59557cf8c3d1a38c63b32
Gustavo G Migacz <gustavomigacz@gmail.com>
2024-02-09 21:04:42 -0300

    add navbar mobile and desktop

--- app/(navbar)/Navbar.tsx
+++ app/(navbar)/Navbar.tsx
@@ -1,27 +1,32 @@
 'use client';
 
+import BurguerMobileIcon from '@/public/svg/BurguerMobileIcon';
 import NavbarIcon from '@/public/svg/NavbarIcon';
 import Link from 'next/link';
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
+import cn from 'clsx';
+import { motion } from 'framer-motion';
 
 export default function Navbar() {
   const [mobileOpen, setMobileOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMobileOpen((prev) => !prev);
-  };
-
   const closeNavbar = () => {
     setMobileOpen(false);
+  };
+
+  const toggleMenu = () => {
+    setMobileOpen((prev) => !prev);
   };
 
   /////////////NavbarDesktop
   return (
     <>
       <nav>
-        <div className='px-[100px] py-6 flex justify-between items-center'>
-          <NavbarIcon />
-          <ul className='text-base md:flex gap-10'>
+        <div className='max-w-[1272px] w-full fixed top-0 left-0 mx-auto md:px-8 px-6 pt-12 flex justify-between items-center z-[9999]'>
+          <Link href={'#'}>
+            <NavbarIcon />
+          </Link>
+          <ul className='text-base md:flex hidden gap-10'>
             <li>
               <Link href={'#'}>HOME</Link>
             </li>
@@ -32,8 +37,76 @@ export default function Navbar() {
               <Link href={'#'}>CONTACT US</Link>
             </li>
           </ul>
+          {/* nav mobile */}
+          <button
+            className={cn(
+              'md:hidden flex burguer-menu',
+              mobileOpen ? 'burguer-menu-open' : 'burguer-menu-closed'
+            )}
+            onClick={toggleMenu}
+          >
+            <BurguerMobileIcon />
+          </button>
+          {/* nav mobile */}
         </div>
+        {mobileOpen ? (
+          <div className='fixed h-full w-full top-0 left-0 text-center bg-[#050505] flex flex-col z-50 justify-center md:hidden pt-[40px]'>
+            <ul className='flex flex-col gap-8 mb-10'>
+              <NavMobileLink href={'#home'} onClick={closeNavbar} delay={0.1}>
+                Home
+              </NavMobileLink>
+              <NavMobileLink href={'#about'} onClick={closeNavbar} delay={0.2}>
+                About Us
+              </NavMobileLink>
+              <NavMobileLink
+                href={'#portfolio'}
+                onClick={closeNavbar}
+                delay={0.3}
+              >
+                Portfolio
+              </NavMobileLink>
+            </ul>
+          </div>
+        ) : null}
       </nav>
     </>
   );
 }
+
+interface NavMobileLinkProps {
+  children: string;
+  href: string;
+  onClick: MouseEventHandler<HTMLAnchorElement>;
+  delay: number;
+}
+
+const NavMobileLink = ({
+  children,
+  href,
+  onClick,
+  delay,
+}: NavMobileLinkProps) => {
+  return (
+    <motion.li
+      initial={{
+        y: 60,
+        opacity: 0,
+      }}
+      animate={{
+        y: 0,
+        opacity: 1,
+      }}
+      transition={{
+        delay: delay,
+      }}
+    >
+      <a
+        href={href}
+        className='inline-block text-[#ffffff] font-header font-semibold text-[54px]'
+        onClick={onClick}
+      >
+        {children}
+      </a>
+    </motion.li>
+  );
+};
