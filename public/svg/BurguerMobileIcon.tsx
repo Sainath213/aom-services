816276bda596d0680f1f9fb5a1c34347818ac19d
Gustavo G Migacz <gustavomigacz@gmail.com>
2024-02-09 21:04:31 -0300

    add burguer mobile icon

---
+++ public/svg/BurguerMobileIcon.tsx
@@ -0,0 +1,36 @@
+export default function BurguerMobileIcon() {
+  return (
+    <svg
+      width='30'
+      height='31'
+      viewBox='0 0 30 31'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M3.75 7.70001H26.25'
+        stroke='white'
+        strokeWidth='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        className='topBar'
+      />
+      <path
+        d='M3.75 15.2H26.25'
+        stroke='white'
+        strokeWidth='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        className='middleBar'
+      />
+      <path
+        d='M3.75 22.7H26.25'
+        stroke='white'
+        strokeWidth='2.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        className='bottomBar'
+      />
+    </svg>
+  );
+}
