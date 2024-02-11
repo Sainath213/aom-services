5867ff52bb66bcedbcea9807697329cdcd988b7f
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-11 18:22:44 -0300

    fix merge issues

--- public/svg/NavbarIcon.tsx
+++ public/svg/NavbarIcon.tsx
@@ -7,7 +7,7 @@ export default function NavbarIcon() {
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
     >
-      <g clip-path='url(#clip0_625_78)'>
+      <g clipPath='url(#clip0_625_78)'>
         <mask
           id='mask0_625_78'
           maskUnits='userSpaceOnUse'
