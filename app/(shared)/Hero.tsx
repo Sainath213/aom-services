789d9bd4b824f8a904191ca42b93a5b88dee8674
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-11 20:35:54 -0300

    add Hero mobile styles

--- app/(shared)/Hero.tsx
+++ app/(shared)/Hero.tsx
@@ -4,17 +4,19 @@ import hero_background from '@/public/hero_background.png';
 export default function Hero() {
   return (
     <header className='mx-auto w-full max-w-[1472px] px-3 pt-6 md:px-8'>
-      <div className='relative aspect-[11/5] overflow-hidden rounded-xl md:rounded-3xl'>
+      <div className='relative h-[418px] overflow-hidden rounded-xl md:h-[640px]'>
         <Image
           alt='Manhattan background'
           src={hero_background}
           quality={100}
           className='absolute h-full w-full object-cover'
         />
-        <div className='absolute flex h-full w-full flex-col justify-center'>
-          <div className='mx-auto mb-12 w-full max-w-[1272px] px-3 md:px-8'>
-            <span className='text-white underline'>AOM Services</span>
-            <h1 className='max-w-[680px] font-serif text-6xl font-bold text-white'>
+        <div className='absolute flex h-full w-full flex-col justify-end pb-4 sm:justify-center sm:pb-0'>
+          <div className='mx-auto w-full max-w-[1272px] px-3 md:mb-12 md:px-8'>
+            <span className='tracking-[2.6px] text-white underline'>
+              AOM Services
+            </span>
+            <h1 className='max-w-[680px] font-serif text-5xl font-bold text-white sm:text-6xl'>
               Streamline Filings for Your Business
             </h1>
           </div>
