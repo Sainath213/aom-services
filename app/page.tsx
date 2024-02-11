82a45bde731ccf9fdd77ca9679a8ac28c06bf541
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-11 18:54:25 -0300

    extract Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,12 @@
-import Image from 'next/image';
-import hero_background from '@/public/hero_background.png';
+import Hero from '@/app/(shared)/Hero';
 import Services from '@/app/(shared)/Services';
 
 export default function Home() {
   return (
-    <main className='flex flex-col items-center justify-between'>
+    <main className='flex flex-col items-center justify-between gap-32'>
       <Hero />
       <Services />
     </main>
   );
 }
 
-const Hero = () => {
-  return (
-    <header className='mx-auto w-full max-w-[1472px] px-3 pt-6 md:px-8'>
-      <div className='relative aspect-[11/5] overflow-hidden rounded-xl md:rounded-3xl'>
-        <Image
-          alt='Manhattan background'
-          src={hero_background}
-          quality={100}
-          className='absolute h-full w-full object-cover'
-        />
-        <div className='absolute flex h-full w-full flex-col justify-center'>
-          <div className='px-3 mx-auto mb-12 w-full max-w-[1272px] md:px-8'>
-            <span className='text-white underline'>AOM Services</span>
-            <h1 className='max-w-[680px] text-6xl font-bold text-white'>
-              Streamline Filings for Your Business
-            </h1>
-          </div>
-        </div>
-      </div>
-    </header>
-  );
-};
