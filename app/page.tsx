9049b41b180a52d2f055a983e92e7422cb21ef86
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-11 18:14:15 -0300

    fix hero border

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 const Hero = () => {
   return (
     <header className='mx-auto w-full max-w-[1472px] px-3 pt-6 md:px-8'>
-      <div className='relative aspect-[11/5] overflow-hidden rounded-xl'>
+      <div className='relative aspect-[11/5] overflow-hidden rounded-xl md:rounded-3xl'>
         <Image
           alt='Manhattan background'
           src={hero_background}
@@ -20,7 +20,7 @@ const Hero = () => {
           className='absolute h-full w-full object-cover'
         />
         <div className='absolute flex h-full w-full flex-col justify-center'>
-          <div className='mx-auto mb-12 w-full max-w-[1272px] px-3 md:px-8'>
+          <div className='px-3 mx-auto mb-12 w-full max-w-[1272px] md:px-8'>
             <span className='text-white underline'>AOM Services</span>
             <h1 className='max-w-[680px] text-6xl font-bold text-white'>
               Streamline Filings for Your Business
