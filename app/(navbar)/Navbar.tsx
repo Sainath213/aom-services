9cf63bb8f5c109aa21866d4b4e75f487d7268fe7
Pedro H G Migacz <pedromigacz@gmail.com>
2024-02-11 18:43:37 -0300

    fix navbar alignment

--- app/(navbar)/Navbar.tsx
+++ app/(navbar)/Navbar.tsx
@@ -18,26 +18,24 @@ export default function Navbar() {
     setMobileOpen((prev) => !prev);
   };
 
-  /////////////NavbarDesktop
   return (
-    <>
-      <nav className='w-full fixed top-0 left-0 md:px-8 px-6 pt-12 items-center z-10'>
-        <div className='flex justify-between items-center mx-auto max-w-[1272px] z-50'>
+      <nav className='w-full fixed top-0 left-0 z-10'>
+        <div className='flex justify-between items-center mx-auto md:px-16 px-6 pt-12 max-w-[1336px]'>
           <Link href={'#home'}>
             <NavbarIcon />
           </Link>
           <ul className='text-base md:flex hidden gap-10 text-white'>
             <li>
-              <Link href='#home' className='hover:underline'>HOME</Link>
+              <a href='#home' className='hover:underline'>HOME</a>
             </li>
             <li>
-              <Link href={'#services'} className='hover:underline'>SERVICES</Link>
+              <a href={'#services'} className='hover:underline'>SERVICES</a>
             </li>
             <li>
-              <Link href={'#contact'} className='hover:underline'>CONTACT</Link>
+              <a href={'#contact'} className='hover:underline'>CONTACT</a>
             </li>
           </ul>
-          {/* nav mobile */}
+          {/* mobile trigger button */}
           <button
             className={cn(
               'md:hidden flex burguer-menu z-[9999]',
@@ -47,10 +45,9 @@ export default function Navbar() {
           >
             <BurguerMobileIcon />
           </button>
-          {/* nav mobile */}
         </div>
         {mobileOpen ? (
-          <div className='fixed h-full w-full top-0 left-0 text-center bg-white flex flex-col z-10 justify-center md:hidden pt-[40px]'>
+          <div className='fixed h-full w-full top-0 left-0 text-center bg-white flex flex-col z-20 justify-center md:hidden pt-10'>
             <ul className='flex flex-col gap-8 mb-10'>
               <NavMobileLink href='#home' onClick={closeNavbar} delay={0.1}>
                 Home
@@ -69,9 +66,9 @@ export default function Navbar() {
           </div>
         ) : null}
       </nav>
-    </>
   );
 }
+
 
 interface NavMobileLinkProps {
   children: string;
